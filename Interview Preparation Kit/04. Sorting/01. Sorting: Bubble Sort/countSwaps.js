f6652b580c710c166614fa91d68722261c536152
yano <yano78@gmail.com>
2019-01-09 18:43:20 +0100

    Buble Sort - count swaps initial commit

---
+++ Interview Preparation Kit/04. Sorting/01. Sorting: Bubble Sort/countSwaps.js
@@ -0,0 +1,30 @@
+/**
+ * Created by yano on 9/01/19.
+ */
+
+'use strict';
+
+
+function countSwaps(a) {
+
+}
+
+function main() {
+	let a, result;
+
+	a = [1, 2, 3];
+	result = countSwaps(a);
+	console.log(result);
+
+	a = [3, 2, 1];
+	result = countSwaps(a);
+	console.log(result);
+
+	a = [4, 2, 3, 1];
+	result = countSwaps(a);
+	console.log(result);
+
+/**/
+}
+
+main();
