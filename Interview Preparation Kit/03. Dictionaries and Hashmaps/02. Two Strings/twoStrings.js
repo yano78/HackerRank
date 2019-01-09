f00d0455d654c3f0c736bae1e90f913c15aad3c2
yano <yano78@gmail.com>
2019-01-09 16:23:10 +0100

    Two strings

---
+++ Interview Preparation Kit/03. Dictionaries and Hashmaps/02. Two Strings/twoStrings.js
@@ -0,0 +1,41 @@
+/**
+ * Created by yano on 1/12/19.
+ */
+
+'use strict';
+
+
+function twoStrings(s1, s2) {
+	let chars = 'abcdefghijklmnopqrstuvwxyz'.split('');
+	// let chars = {};
+	let result = false;
+	for (let i of chars) {
+		if (s1.indexOf(i) > -1 && s2.indexOf(i) > -1) {
+			result = true;
+			break;
+		}
+	}
+	return (result ? 'Yes' : 'No');
+}
+
+function main() {
+	let s1, s2, result;
+
+	s1 = 'hello world';
+	s2 = 'hi world';
+	result = twoStrings(s1, s2);
+	console.log(result);
+/*
+	s1 = '';
+	s2 = '';
+	result = freqQuery(s1, s2);
+	console.log(result);
+
+	s1 = '';
+	s2 = '';
+	result = freqQuery(s1, s2);
+	console.log(result);
+*/
+}
+
+main();
