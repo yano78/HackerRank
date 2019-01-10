e76295d04e9d6b28d74e77a63635bf01c98dc180
yano <yano78@gmail.com>
2019-01-10 13:09:18 +0100

    Buble Sort - solution

--- Interview Preparation Kit/04. Sorting/01. Sorting: Bubble Sort/countSwaps.js
+++ Interview Preparation Kit/04. Sorting/01. Sorting: Bubble Sort/countSwaps.js
@@ -6,7 +6,23 @@
 
 
 function countSwaps(a) {
+	let numSwaps = 0;
+	let l = a.length;
 
+	for (let i = 0; i < l; i++) {
+		for (let j = 0; j < l - 1; j++) {
+			if (a[j] > a[j + 1]) {
+				let tmp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = tmp;
+				numSwaps++;
+			}
+		}
+	}
+
+	console.log(`Array is sorted in ${numSwaps} swaps`);
+	console.log(`First Element: ${a[0]}`);
+	console.log(`Last Element: ${a[l - 1]}`);
 }
 
 function main() {
