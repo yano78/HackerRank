3b4592b30cb498f88a38670acda12d63beed0432
yano <yano78@gmail.com>
2019-01-14 20:10:57 +0100

    Maximum Toys - solution

---
+++ Interview Preparation Kit/04. Sorting/03. Sorting: Comparator/maximumToys.js
@@ -0,0 +1,23 @@
+/**
+ * Created by yano on 9/01/19.
+ */
+
+'use strict';
+
+
+function maximumToys(prices, k) {
+
+}
+
+function main() {
+	let prices, k, result;
+
+	prices = [1, 12, 5, 111, 200, 1000, 10];
+	k = 50;
+	result = maximumToys(prices, k);
+	console.log(result);
+
+	/**/
+}
+
+main();
